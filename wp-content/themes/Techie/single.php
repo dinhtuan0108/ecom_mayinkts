6afb294fc2333ff97b9665057f1cadbacfd117fe
TuanDQ <tuandq@lifetimetech.vn>
2013-11-21 23:10:37 +0700

    Update other pages

--- wp-content/themes/Techie/single.php
+++ wp-content/themes/Techie/single.php
@@ -24,13 +24,7 @@
             <?php $theme->hook('content_after'); ?>
         
         </div><!-- #content -->
-        <div class="wrap">
-        <h2><img src="/img/h-sidebox_pickup.gif" alt="PICK UP" width="250" height="16" /></h2>
-        <p><a href="/about/event/Print13/"><img src="/img/img-sidebox_print13.jpg" alt="Ricoh at Print13" width="250" height="100" border="0" /></a></p>
-        <p><a href="/about/company/ataglance/"><img src="/img/img-sidebox_ataglance.jpg" alt="Ricoh at a Glance" width="250" height="100" border="0" /></a></p>
-        
-        
-        <!--wrap--></div>
+      
         <?php get_sidebars(); ?>
         
         <?php $theme->hook('main_after'); ?>
